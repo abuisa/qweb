c5e09a5f1e5cafe4995279cc2ee70a40c8a07159
elyas <elyas.uii14@gmail.com>
2017-10-14 18:34:30 +0700

    Fix rstr and index 1834

--- rstr.py
+++ rstr.py
@@ -1,3 +1,16 @@
+'''
+Script ini untuk mengubah halaman --Ayat-Koleksi.htm-- menjadi --index.html--
+digunakan di windows CMD, 
+How to : 
+	1. buka CMD, 
+	2. Masuk ke Direktori script ini berada dengan perintah : --CD X:\Contoh\Folder\qweb--
+	3. ketik perintah : --python rstr.py-- lalu ENTER 
+	4. Selesai,
+-----------------------
+NB : 	Jika mau Update Ayat Koleksi, Lakukan Update pada file --Ayat-Koleksi.htm-- 
+		Setelah update selesai, lakukan instruksi sebagaimana di --How to--
+'''
+
 def thtm(f,s):
 	f = open(f,"+a") #opens file with name of "test.txt"
 	f.write(s)
@@ -10,6 +23,8 @@ for l in open('Ayat-Koleksi.htm'):
 	px = "'px'"
 	ll = l.replace('target="mframe"',r'onclick="myFunction(\''+dn+'\')" target="'+dn+'"')
 	ll = ll.replace('\\','')
+	ll = ll.replace('index.html','old-index.htm')
+	ll = ll.replace('New Page','Old Page')
 	thtm('index.html',ll)
-	if 'a href=' in ll:
+	if 'a href=' in ll and 'Al-Quran' in ll:
 		thtm('index.html','<div id="'+dn+'" style="display:none;"><iframe name="'+dn+'" src="" style="border: none; " width="100%" onload="this.style.height=this.contentDocument.body.scrollHeight +'+px+';"></iframe></div>\n')
